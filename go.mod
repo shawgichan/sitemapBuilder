dfcad0bc67ed8f1dc3070cde80c13f9c27b183fb
Shawgi Hafiz <shawgichan@gmail.com>
2025-01-22 08:23:33 +0400

    get webpage

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sitemapBuilder
+
+go 1.23.1
--- main.go
+++ main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
+
+/*
+	1. Get the webpage
+	2. Parse all the links on the page
+	3. Build proper urls with our links
+	4. Filter out any links with a different domain
+	5. Find all pages (BFS)
+	6. Print out XML
+*/
 
 func main() {
 	urlFlag := flag.String("url", "", "url of the website to scrape")
 	flag.Parse()
 
 	fmt.Println(*urlFlag)
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
 
-	/*
-		1. Get the webpage
-		2. Parse all the links on the page
-		3. Build proper urls with our links
-		4. Filter out any links with a different domain
-		5. Find all pages (BFS)
-		6. Print out XML
-	*/
 }
