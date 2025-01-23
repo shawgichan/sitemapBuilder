807f316eac4f8ef7ad245f56e855e5bbf33ce7d3
Shawgi Hafiz <shawgichan@gmail.com>
2025-01-23 08:26:08 +0400

    use link custom package and extract links

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module sitemapBuilder
 
 go 1.23.1
+
+require (
+	github.com/shawgichan/link v0.0.1 // indirect
+	golang.org/x/net v0.34.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
+
+	"github.com/shawgichan/link"
 )
 
 /*
@@ -27,6 +29,27 @@ func main() {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	links, _ := link.Parse(resp.Body)
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+
+		}
+	}
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 
 }
