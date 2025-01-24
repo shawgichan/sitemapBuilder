7dfb9bad46520c6f71fd936719eda3c191e0b1fc
Shawgi Hafiz <shawgichan@gmail.com>
2025-01-24 06:00:41 +0400

    refactor

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -22,9 +23,16 @@ import (
 func main() {
 	urlFlag := flag.String("url", "", "url of the website to scrape")
 	flag.Parse()
-
 	fmt.Println(*urlFlag)
-	resp, err := http.Get(*urlFlag)
+
+	pages := get(*urlFlag)
+	for _, page := range pages {
+		fmt.Println(page)
+	}
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
 	if err != nil {
 		panic(err)
 	}
@@ -36,20 +44,19 @@ func main() {
 		Host:   reqUrl.Host,
 	}
 	base := baseUrl.String()
+	return hrefs(resp.Body, base)
+}
 
-	links, _ := link.Parse(resp.Body)
-	var hrefs []string
+func hrefs(r io.Reader, base string) []string {
+	links, _ := link.Parse(r)
+	var ret []string
 	for _, l := range links {
 		switch {
 		case strings.HasPrefix(l.Href, "/"):
-			hrefs = append(hrefs, base+l.Href)
+			ret = append(ret, base+l.Href)
 		case strings.HasPrefix(l.Href, "http"):
-			hrefs = append(hrefs, l.Href)
-
+			ret = append(ret, l.Href)
 		}
 	}
-	for _, href := range hrefs {
-		fmt.Println(href)
-	}
-
+	return ret
 }
